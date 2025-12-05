4c95afc0c9ab511501a086db8f6c7cd2498bf0dc
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2025-12-05 16:47:51 +0300

    fix: remove base config for vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "reliable-homeo-clinic-24",
+  // base: "reliab",
   server: {
     host: "::",
     port: 8080,
